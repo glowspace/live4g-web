146a3232894d3b7335a681e608725b9509daac50
Barbora Kuchaříková <kucharikova@softici.cz>
2021-03-18 22:14:56 +0100

    menu updated

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,7 +3,7 @@
     <p class="mb-0 mx-auto">Líbí se vám myšlenka projektu Live 4G? <a href="" class="btn-link">Podpořte nás!</a></p>
 </nav>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-black px-5 shadow">
+<nav class="navbar navbar-expand-lg navbar-dark bg-black px-lg-5 px-2 shadow">
     <a class="navbar-brand" href="/"><img src="{{asset('/images/live4g-white.png')}}" height="50px"></a>
     <button class="navbar-toggler"
             type="button"
@@ -28,7 +28,7 @@
             </li>
         </ul>
         <form action="">
-            <div class="p-1 bg-light rounded rounded-pill shadow-sm my-auto">
+            <div class="p-1 bg-light rounded rounded-pill shadow-sm my-lg-auto mb-3">
                 <div class="input-group">
                     <input type="search" placeholder="Vyhledat..." class="form-control border-0 bg-transparent">
                     <div class="input-group-append">
@@ -38,6 +38,19 @@
                 </div>
             </div>
         </form>
+        <ul class="navbar-nav mr-0 mt-2 mt-lg-0 socials">
+            <li class="nav-item my-auto">
+                <a class="nav-link" href="https://www.facebook.com/live4gcz"><i class="fab fa-facebook"></i></a>
+            </li>
+{{--            <li class="nav-item my-auto">--}}
+{{--                <a class="nav-link" href="https://www.youtube.com/channel/UCaQ72CWpjrBR1e5Ub6-Mqcg"><i class="fab fa-youtube"></i></a>--}}
+{{--            </li>--}}
+            <li class="nav-item my-auto">
+                <a class="nav-link" href="https://www.instagram.com/live4gcz/?fbclid=IwAR2amkGS-7UW2-Yu46SlJPIvXNdC01B2dtWeNgIfcfguKroTLw-6fvqe8ag"><i class="fab fa-instagram"></i></a>
+            </li>
+
+
+        </ul>
     </div>
 
 </nav>
