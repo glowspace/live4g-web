a23567f8ec7f00345037c56aa53b94d726c2e969
Barbora Kuchaříková <kucharikova@softici.cz>
2021-03-18 22:32:41 +0100

    footer draft

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,19 @@
+<section class="bg-black" id="footer">
+    <div class="container-fluid px-5 py-5">
+        <div class="row">
+            <div class="col-lg-3 col-12">
+                <h5><b>Pořady</b></h5>
+                <a>Chvály 4G</a><br>
+                <a>Minuta 4G</a><br>
+                <a>Hřešíme s Kubou</a><br>
+                <a>Jiří Dyčka</a>
+            </div>
+            <div class="col-lg-3 col-12">
+                <h5><b>O nás</b></h5>
+                <a>O Live4G</a><br>
+                <a>Body k zamyšlení</a><br>
+                <a>Kontakt</a>
+            </div>
+        </div>
+    </div>
+</section>
