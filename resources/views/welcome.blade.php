a2ea9a275457495790dda46744ad1983dfd243db
Barbora Kuchaříková <kucharikova@softici.cz>
2021-03-18 21:22:17 +0100

    live video badge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
                 <div class="col-lg-2 col-md-4 col-sm-6 col-12">
                     <div class="item">
                         <span class="time-badge">8:15</span>
+                        <span class="live-badge"><i class="fas fa-circle text-danger blinking"></i> Živě</span>
                         <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
                         <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
                     </div>
@@ -59,7 +60,7 @@
                     <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
                 </div>
             </div>
-            <div class="row">
+            <div class="row my-5">
                 <div class="col-12 mb-3">
                     <h3 class="d-inline-block"><b>Hřešíme s Kubou</b></h3>
                     <a href="/porad" class="d-inline-block text-uppercase more font-weight-bold float-right">Ukázat vše
@@ -67,33 +68,109 @@
                             class="fas fa-chevron-right"></i></a>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-6 col-12">
-                    <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
-                    <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    <div class="item">
+                        <span class="time-badge">21:48</span>
+                        <span class="new-badge">Nové</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
                     <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-6 col-12">
-                    <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
-                    <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    <div class="item">
+                        <span class="time-badge">8:15</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
                     <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-6 col-12">
-                    <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
-                    <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    <div class="item">
+                        <span class="time-badge">12:35</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
                     <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-6 col-12">
-                    <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
-                    <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    <div class="item">
+                        <span class="time-badge">21:01</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
                     <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-6 col-12">
-                    <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
-                    <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    <div class="item">
+                        <span class="time-badge">21:48</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
                     <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
                 </div>
                 <div class="col-lg-2 col-md-4 col-sm-6 col-12">
-                    <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
-                    <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    <div class="item">
+                        <span class="time-badge">21:48</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
+                    <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
+                </div>
+            </div>
+            <div class="row my-5">
+                <div class="col-12 mb-3">
+                    <h3 class="d-inline-block"><b>Jiří Dyčka</b></h3>
+                    <a href="/porad" class="d-inline-block text-uppercase more font-weight-bold float-right">Ukázat vše
+                        <i
+                            class="fas fa-chevron-right"></i></a>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="item">
+                        <span class="time-badge">21:48</span>
+                        <span class="new-badge">Nové</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
+                    <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="item">
+                        <span class="time-badge">8:15</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
+                    <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="item">
+                        <span class="time-badge">12:35</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
+                    <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="item">
+                        <span class="time-badge">21:01</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
+                    <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="item">
+                        <span class="time-badge">21:48</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
+                    <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="item">
+                        <span class="time-badge">21:48</span>
+                        <img src="{{asset('/images/epizoda.png')}}" width="100%" class="border-rounded">
+                        <h5 class="mt-3 mb-1"><b>Název videa</b></h5>
+                    </div>
                     <p class="small">Název pořadu <span class="float-right">dd/mm/rr</span></p>
                 </div>
             </div>
