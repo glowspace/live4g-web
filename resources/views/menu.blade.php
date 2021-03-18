3661edc2169d5838b8461a60e2714c701e8e2cd3
Barbora Kuchaříková <kucharikova@softici.cz>
2021-03-18 17:50:15 +0100

    fe draft

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,32 @@
+{{--Menu--}}
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-5 shadow">
+    <a class="navbar-brand" href="/">LOGO</a>
+    <button class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Domů</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/nase-sluzby">Naše služby</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/galerie">Fotogalerie</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/kontakt">Kontakt</a>
+            </li>
+
+        </ul>
+    </div>
+
+</nav>
